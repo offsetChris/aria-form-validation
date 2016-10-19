9e0bb6e3f132a501dbb171568f78b435547101a4
offsetChris <christina@offsetchris.com>
2016-10-19 17:23:43 -0500

    added warning style and simple validation

--- js/main.js
+++ js/main.js
@@ -7,15 +7,44 @@ function validateForm() {
 
 	$('[aria-required="true"]').each(function(){
 		if($(this).is('fieldset')){
-
+			if(!$(this).find('input:checked').length > 0){
+                $(this).attr('aria-invalid',true);
+                var errortxt = $(this).find('legend').text();
+                errorSummary.append('<li>'+errortxt+'</li>');
+                $(this).append('<span role="alert" class="error-message">Please choose an option</span>');
+            } else {
+                $(this).attr('aria-invalid', false);
+            }
 		}
 		if($(this).is('input[type="text"],textarea')){
-
+			if($(this).val() == ''){
+                $(this).attr('aria-invalid',true);
+                var errortxt = $(this).prev('label').text();
+                errorSummary.append('<li>'+errortxt+'</li>');
+                $(this).parent().append('<span role="alert" class="error-message">Please enter text</span>');
+            } else {
+                $(this).attr('aria-invalid', false);
+            }
 		}
 		if($(this).is('select')){
-			
+			if($(this).val() < 0 ){
+                $(this).attr('aria-invalid', true);
+                var errortxt = $(this).parents('.select-wrapper').prev('label').text();
+                errorSummary.append('<li>'+errortxt+'</li>');
+                $(this).parent().append('<span role="alert" class="error-message">Please select an option</span>');
+            } else {
+                $(this).attr('aria-invalid',false);
+            }
 		}
 	});
 
+	if($('[aria-invalid="true"]').length > 0 ){
+        console.log('Errors!');
+        $('#error-summary').fadeIn();
+        window.scrollTo(0,0);
+    } else {
+        // ajax post data
+        console.log('Send form!')
+    }  
 
 }
